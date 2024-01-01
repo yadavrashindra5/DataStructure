2bfbc0f068e194a94523a0ead579f9ccedba543a
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-01-01 18:31:04 +0530

    feat(bed):add node file

--- src/main/java/org/example/lineardatastructure/linkedlist/Node.java
+++ src/main/java/org/example/lineardatastructure/linkedlist/Node.java
@@ -1,2 +1,10 @@
-package org.example.lineardatastructure.linkedlist;public class Node {
+package org.example.lineardatastructure.linkedlist;
+
+public class Node<T> {
+    public T data;
+    public Node<T> next;
+    Node(T data){
+        this.data=data;
+        this.next=null;
+    }
 }
